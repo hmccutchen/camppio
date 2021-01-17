ef60a00734b88575f00c0d2ded78576107901fa5
hmccutchen <mccutchen.hassan@gmail.com>
2021-01-17 01:00:00 -0500

    create author controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'author/index'
+  get 'author/show'
+  get 'author/new'
   root to: 'book#index'
   get 'book/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
---
+++ test/controllers/author_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class AuthorControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get author_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get author_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get author_new_url
+    assert_response :success
+  end
+
+end
