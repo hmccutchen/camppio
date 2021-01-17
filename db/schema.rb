1a4b1cf27dcc2db4a53436c7d59473e3cd36d94a
hmccutchen <mccutchen.hassan@gmail.com>
2021-01-16 22:33:40 -0500

    add author

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_023127) do
+ActiveRecord::Schema.define(version: 2021_01_17_024900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.integer "birth_year"
+    t.bigint "books_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["books_id"], name: "index_authors_on_books_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_01_17_023127) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "authors", "books", column: "books_id"
 end
